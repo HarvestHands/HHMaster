ce1a2286d9d741fe6330457749af4c1e74d08213
Harvest Hands Master <harvesthandsgame@gmail.com>
2016-09-07 09:25:53 +1000

    Color Effects

---
+++ Harvest Hands Prototyping/Assets/Scripts/pickupable2.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pickupable2 : MonoBehaviour
+{
+    public enum pickupType
+    {
+        PICKUP = 0,
+        INTERACT = 1,
+        BUY = 2,
+    }
+
+
+    public bool hit;
+    //private Texture tex;
+    public pickupType type;
+    public float FresnelAmount;
+    public float FresnelDecrease;
+    //public float FresnalRate;
+    public float FresnelIncrease;
+    //private float FresnalTimer;
+    // Use this for initialization
+    void Start()
+    {
+		GetComponent<Renderer> ().sharedMaterial = GetComponent<Renderer> ().material;
+
+
+
+        //Debug.Log(tex);
+
+        //FresnelDrop = 0.1f; // drop amount per rate
+        //FresnalRate = 0.1f; // seconds
+        //FresnelIncrease = 0.5f; // increase amount per rate
+
+        //FresnalTimer = FresnalRate;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {        
+        if (hit)
+        {
+ 
+            FresnelAmount += Time.deltaTime * FresnelIncrease;
+            FresnelAmount = Mathf.Clamp(FresnelAmount, 0, 1);
+            //GetComponent<Renderer>().sharedMaterial.SetFloat("_FresnelAmount", FresnelAmount);
+            switch (type)
+            {
+                case pickupType.PICKUP:
+                    GetComponent<Renderer>().sharedMaterial.SetColor("_FresnelColour", ColorSingleton.instance.PickUp);
+                    GetComponent<Renderer>().sharedMaterial.SetFloat("_FresnelAmount", FresnelAmount);// FresnelAmount
+                    break;
+                case pickupType.INTERACT:
+                    GetComponent<Renderer>().sharedMaterial.SetColor("_FresnelColour", ColorSingleton.instance.Interact);
+                    GetComponent<Renderer>().sharedMaterial.SetFloat("_FresnelAmount", FresnelAmount);// FresnelAmount
+                    break;
+                case pickupType.BUY:
+                    GetComponent<Renderer>().sharedMaterial.SetColor("_FresnelColour", ColorSingleton.instance.Buy);
+                    GetComponent<Renderer>().sharedMaterial.SetFloat("_FresnelAmount", FresnelAmount);// FresnelAmount
+                    break;
+            }
+        }
+        else
+        {
+            FresnelAmount -= Time.deltaTime * FresnelDecrease;
+            FresnelAmount = Mathf.Clamp(FresnelAmount, 0, 1);
+            GetComponent<Renderer>().sharedMaterial.SetFloat("_FresnelAmount", FresnelAmount);
+        }
+
+        hit = false;
+    }
+}
--- Harvest Hands Prototyping/Assets/Scripts/waterNoiseTest.cs
+++ Harvest Hands Prototyping/Assets/Scripts/waterNoiseTest.cs
@@ -4,41 +4,45 @@ using System.Collections;
 [ExecuteInEditMode]
 public class waterNoiseTest : MonoBehaviour 
 
+{
+	public float perlinScale;
+	public float waveSpeed;
+	public float waveHeight;
+
+	private Mesh mesh;
+		
+	public GameObject splash;
+
+	void Update()
 	{
-		public float perlinScale;
-		public float waveSpeed;
-		public float waveHeight;
-
-		private Mesh mesh;
-
-
-		void Update()
-		{
-			AnimateMesh();
-		}
-
-		void AnimateMesh()
-		{
-			if ( !mesh )
-				mesh = GetComponent< MeshFilter >().mesh;
-
-			Vector3[] vertices = mesh.vertices;
-
-			for (int i = 0; i < vertices.Length; i++)
-			{
-				float pX = ( vertices[i].x * perlinScale ) + ( Time.timeSinceLevelLoad * waveSpeed );
-				float pZ = ( vertices[i].z * perlinScale ) + ( Time.timeSinceLevelLoad * waveSpeed );
-
-				vertices[i].y = ( Mathf.PerlinNoise( pX, pZ ) - 0.5f ) * waveHeight;
-			}
-
-			mesh.vertices = vertices;
-		}
-
-	/*void OnCollisionStay(Collision terrain){
-		foreach (ContactPoint contact in terrain.contacts) {
-			print (contact.thisCollider.name + "hit" + contact.otherCollider.name);
-			Debug.DrawRay (contact.point, contact.normal, Color.white);
-		}
-	}*/
+		AnimateMesh();
 	}
+
+	void OnCollision(Collider col){
+
+		if (col.gameObject.GetComponent<Pickupable> () != null) {
+
+			Instantiate (splash, col.transform.position, col.transform.rotation );
+			Debug.Log ("Splash");
+		}
+
+	}
+
+	void AnimateMesh()
+	{
+		if ( !mesh )
+			mesh = GetComponent< MeshFilter >().mesh;
+	
+		Vector3[] vertices = mesh.vertices;
+
+		for (int i = 0; i < vertices.Length; i++)
+		{
+			float pX = ( vertices[i].x * perlinScale ) + ( Time.timeSinceLevelLoad * waveSpeed );
+			float pZ = ( vertices[i].z * perlinScale ) + ( Time.timeSinceLevelLoad * waveSpeed );
+
+			vertices[i].y = ( Mathf.PerlinNoise( pX, pZ ) - 0.5f ) * waveHeight;
+		}
+
+		mesh.vertices = vertices;
+	}
+}
