d42581b67c31d8e6c148d3c7e2946ecb92a1f53e
Harvest Hands Master <harvesthandsgame@gmail.com>
2016-09-12 13:41:06 +1000

    New Level second commit because first one bugged out and didn't upload shit :(

---
+++ Harvest Hands Prototyping/Assets/Scripts/waterBox.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class waterBox : MonoBehaviour {
+
+	public GameObject underWaterUI;
+	public float drownTimer = 20f;
+	public Slider oxySlider;
+	public GameObject spawnPoint;
+
+	void Start(){
+
+		oxySlider.maxValue = 20f;
+
+	}
+
+	void OnTriggerStay(Collider plr){
+
+		if (plr.gameObject.tag == "Player") {
+
+			underWaterUI.GetComponent<Canvas> ().enabled = true;
+		} 
+
+		drownTimer -= Time.deltaTime;
+		if (drownTimer < 0) {
+
+			plr.transform.position = spawnPoint.transform.position;
+			Debug.Log ("Dead af lol");
+
+		}
+
+		oxySlider.value = drownTimer;
+	}
+
+	void OnTriggerExit(Collider plr){
+
+		underWaterUI.GetComponent<Canvas> ().enabled = false;
+
+		drownTimer = 20f;
+	}
+}
