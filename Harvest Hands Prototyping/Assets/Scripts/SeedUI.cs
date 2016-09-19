8781b2b2fae882357bc8197ee66313f5b0b556db
Harvest Hands Master <harvesthandsgame@gmail.com>
2016-09-19 14:59:11 +1000

    Adjusted seed UI's

--- Harvest Hands Prototyping/Assets/Scripts/SeedUI.cs
+++ Harvest Hands Prototyping/Assets/Scripts/SeedUI.cs
@@ -1,11 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+
 public class SeedUI : MonoBehaviour {
 
+	public Font textFont;
 
-     Text SeedNumber;
-     Text SeedType;
+	public Text SeedNumber;
+	public Text SeedType;
 
 
 	// Use this for initialization
@@ -14,6 +16,19 @@ public class SeedUI : MonoBehaviour {
         SeedNumber =  this.GetComponentsInChildren<Text>()[0]; //("SBAMT");
         SeedType = this.GetComponentsInChildren<Text>()[1]; 
 
+		/*SeedNumber.font = textFont;
+		SeedNumber.fontSize = 18;
+		SeedNumber.color = Color.yellow;
+		SeedNumber.horizontalOverflow = HorizontalWrapMode.Overflow;
+		SeedNumber.verticalOverflow = VerticalWrapMode.Overflow;
+		SeedNumber.lineSpacing = 5;
+
+		SeedType.font = textFont;
+		SeedType.fontSize = 18;
+		SeedType.color = Color.yellow;
+		SeedType.horizontalOverflow = HorizontalWrapMode.Overflow;
+		SeedType.verticalOverflow = VerticalWrapMode.Overflow;
+		SeedType.lineSpacing = 5;*/
 	}
 	
 	// Update is called once per frame
