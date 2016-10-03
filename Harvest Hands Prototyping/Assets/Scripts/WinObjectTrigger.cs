910c666d465e4e29ca07e3860649534b258a750f
Nicholas Byrne <nicholas.byrne@ad.aie.edu>
2016-10-03 16:59:20 +1100

    Bed(skip to morning) + win condition + Leaderboard saving

---
+++ Harvest Hands Prototyping/Assets/Scripts/WinObjectTrigger.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class WinObjectTrigger : MonoBehaviour
+{
+    public bool gameWon = false;
+    public int numberOfHighScores = 5;
+    // Use this for initialization
+    void Awake ()
+    {
+        Time.timeScale = 1f;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (!gameWon)
+            return;
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            SceneManager.LoadScene(0);
+        }
+        
+
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        //if collider is WinObject
+        if (col.GetComponent<WinObject>() != null)
+        {
+            //Win Game
+            gameWon = true;
+            Time.timeScale = 0.0001f;
+            int finalScore = 1;
+
+            PlayerPrefs.SetFloat("finalScore1", finalScore);
+            //if a new highscore
+            if (finalScore > PlayerPrefs.GetFloat("finalScore" + (numberOfHighScores - 1)))
+            {
+                //load old score order
+                List<float> highscores = new List<float>();
+                for (int i = 0; i < numberOfHighScores; ++i)
+                {
+                    highscores.Add(PlayerPrefs.GetFloat("finalScore" + i.ToString(), 0));
+                    
+                }
+                //add new highscore to list
+                highscores.Add(finalScore);
+
+                //calculate new highscore order
+                highscores.Sort((x, y) => y.CompareTo(x));
+
+                //Save new list order
+                for (int i = 0; i < numberOfHighScores; ++i)
+                {
+                    PlayerPrefs.SetFloat("finalScore" + i.ToString(), highscores[i]);
+                }
+            }
+
+
+        }
+    }
+}
