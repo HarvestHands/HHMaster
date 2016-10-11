2ad7108ad384e1833ef42f3fe5c96181a08ce831
Nicholas Byrne <nicholas.byrne@ad.aie.edu>
2016-10-11 12:33:07 +1100

    Sundial and stuff

---
+++ Harvest Hands Prototyping/Assets/Sundial.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Sundial : MonoBehaviour
+{
+    DayNightController dayNightController;
+    public float rotationOffset = 0;
+    public RectTransform rectTransform;
+
+	// Use this for initialization
+	void Start ()
+    {
+        dayNightController = FindObjectOfType<DayNightController>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //Vector3 rotation = new Vector3(0, dayNightController.currentTimeOfDay * 360f + rotationOffset, 0);
+        rectTransform.localRotation = Quaternion.Euler(0.0f, 0, dayNightController.currentTimeOfDay * 360f + rotationOffset);
+        //rectTransform.localRotation.eulerAngles.z.Set(rotation);
+        //rectTransform.localRotation.z = dayNightController.currentTimeOfDay * 360f;
+    }
+}
