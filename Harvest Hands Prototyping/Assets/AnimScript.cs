71a8bb21694a5d9d35660d177a08a807218f9ae3
Joshua Barber <joshua.barber@ad.aie.edu>
2016-10-12 11:26:45 +1100

    Added lerp to held obj to move smoothly when held buuuuuuut it doesnt pull back when you throw Added staff into player prefab with walk anim constantly playing Tweaked headbob in player script Also more butts

---
+++ Harvest Hands Prototyping/Assets/AnimScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimScript : MonoBehaviour
+{
+    [SerializeField]Animator anim;
+    [SerializeField]float runSpeed;
+
+    enum stateMachine
+    {
+        noState = -1,
+        idle,
+        walk,
+        run
+    };
+
+    stateMachine sMach;
+
+    // Use this for initialization
+    void Start ()
+    {
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        anim.SetBool("IsWalking", true); //changes based ons true or false
+        anim.SetFloat("New Float", runSpeed); //changes when over or under a float value
+        sMach = 0;
+        anim.SetInteger("State", (int)sMach);//changes based on state
+        anim.Play("WalkCycle");//just plays animation
+
+
+        
+
+	}
+}
