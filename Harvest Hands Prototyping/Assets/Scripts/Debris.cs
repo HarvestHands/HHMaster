70bd05cf8ca50870e688ccb0ac1d776ffbdf5e43
unknown <victorbrkic@hotmail.com>
2016-10-17 02:00:53 +1100

    Phew , well this is a bit of an update So here's the goods : 1. Made button and script to insta sell the catapault's goods 2.Fixed debris to reset to original position 3.Made items bought at shop (by button) spawn at farm after night 4.Added death penalty reset after night , and added the penalty for running as well 5.mushroom respawn : well i couldn't unfortunatley get this working without the mushrooms tag set to mushroom , i tried using lists and i tried different methods of getting the damn shooms to go to shroom heaven but it just wouldn't work , im sorry lads :(

--- Harvest Hands Prototyping/Assets/Scripts/Debris.cs
+++ Harvest Hands Prototyping/Assets/Scripts/Debris.cs
@@ -5,7 +5,7 @@ public class Debris : MonoBehaviour
 {
     public int requiredShredderTier = 1;
     Vector3 startpos;
-
+    Vector3 SavedPos;
     // Use this for initialization
     void Start()
     {
@@ -29,16 +29,27 @@ public class Debris : MonoBehaviour
 
 
         startpos = new Vector3(355.33f, 231.85f, 82.48006f);
+        SavedPos = new Vector3(0, 0, 0);
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
 
-        if (GameObject.Find("GameManager").GetComponent<DayNightController>().currentTimeOfDay >= 0.75)
+
+        if (GameObject.Find("GameManager").GetComponent<DayNightController>().currentTimeOfDay >= 0.75 && GameObject.Find("GameManager").GetComponent<DayNightController>().currentTimeOfDay <= 0.76)
         {
-            this.gameObject.GetComponent<Rigidbody>().MovePosition(startpos);
+            this.gameObject.GetComponent<Rigidbody>().MovePosition(SavedPos);
         }
+
+
+
+        if (GameObject.Find("GameManager").GetComponent<DayNightController>().currentTimeOfDay >= 0.26 && GameObject.Find("GameManager").GetComponent<DayNightController>().currentTimeOfDay <= 0.27)
+        {
+            SavedPos = new Vector3(0, 0, 0);
+            SavedPos += this.gameObject.GetComponent<Rigidbody>().position;
+        }
+
 
 	}
 }
